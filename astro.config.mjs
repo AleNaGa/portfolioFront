bf8ee708a962d2a5d44b0e5b26d6897bf7f1dcaa
K1MP4 <alextiloup@gmail.com>
2024-12-01 19:53:52 +0100

    run build perfect

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,14 +5,15 @@ import tailwind from '@astrojs/tailwind';
 
 import react from '@astrojs/react';
 
+import node from '@astrojs/node';
+
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'hybrid',
+  output: 'server', // Salida para aplicaciones con contenido dinámico
+  adapter: node({
+    mode: 'standalone', // Configuración ideal para Azure
+  }),
   integrations: [tailwind(), react()],
-  outDir: './dist', // Carpeta de salida
-  build: {
-    format: 'directory',
-  },
   
 });
